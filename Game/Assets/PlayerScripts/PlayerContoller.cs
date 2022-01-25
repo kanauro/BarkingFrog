5425602542569567ce077f7adc8f288f62488657
pavel-semenov-1 <q7.pavel@gmail.com>
2022-01-25 19:29:20 +0100

    make animation irrelevant to the movement

--- Game/Assets/PlayerScripts/PlayerContoller.cs
+++ Game/Assets/PlayerScripts/PlayerContoller.cs
@@ -1,73 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(CharacterController), typeof(Animator))]
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(Animator))]
 public class PlayerContoller : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float Speed = 5f;
-    public float JumpHeight = 2f;
-    public float Gravity = 9.81f;
-    public float GroundDistance = 0.2f;
-    public float DashDistance = 5f;
-    public LayerMask Ground;
-    public Vector3 Drag;
-
-    private CharacterController _controller;
-    //private Vector3 _velocity;
+    public float speed = 5f;
+    public float jumpHeight = 10f;
+    public float groundDistance = 0.2f;
+    public LayerMask ground;
+    
     private bool _isGrounded = true;
-    private bool dir_left = true;
-    private int jump = 0;
+    private int _jump = 0;
     private Transform _groundChecker;
 
-    private Animator animator;
-    private List<string> items;
+    private Animator _animator;
+    private List<string> _items;
+    private Rigidbody _rigidbody;
+    private static readonly int IsJumping = Animator.StringToHash("isJumping");
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
 
     void Start()
     {
-        _controller = GetComponent<CharacterController>();
         _groundChecker = transform;
-        animator = GetComponent<Animator>();
-        items = new List<string>();
+        _animator = GetComponent<Animator>();
+        _items = new List<string>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        _isGrounded = Physics.CheckSphere(_groundChecker.position, groundDistance, ground, QueryTriggerInteraction.Ignore);
 
-        float dir = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        float x = 0;
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            this.transform.transform.localRotation = Quaternion.Euler(new Vector3(0, -90, 0));
-            if (dir > 0) dir *= -1;
-        } else if (Input.GetKeyDown(KeyCode.RightArrow))
+            transform.localRotation = Quaternion.Euler(new Vector3(0, -90, 0));
+            x = -1 * speed * Time.deltaTime;
+        } 
+        else if (Input.GetKeyDown(KeyCode.D))
         {
-            this.transform.transform.localRotation = Quaternion.Euler(new Vector3(0, 90, 0));
-            if (dir < 0) dir *= -1;
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 90, 0));
+            x = speed * Time.deltaTime;
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow) && _isGrounded)
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
-            animator.SetBool("isJumping", true);
-            jump = 1;
-            //_velocity.y = JumpHeight;
+            _animator.SetBool(IsJumping, true);
+            _jump = 1;
         }
-        Vector3 move = new Vector3(dir * Speed * Time.deltaTime, 0, JumpHeight * jump);
-        print(move);
-        _controller.Move(move);
-        jump = 0;
-        transform.position = new Vector3(transform.position.x, transform.position.y, -1.1f); //debug
-        animator.SetBool("isRunning", Input.GetAxisRaw("Horizontal") != 0);
-        animator.SetBool("isJumping", !_isGrounded);
+
+        Vector3 move = new Vector3(x, _jump*jumpHeight*Time.deltaTime, 0);
+        _rigidbody.velocity = move;
+        _jump = 0;
+        _animator.SetBool(IsRunning, Input.GetAxisRaw("Horizontal") != 0);
+        _animator.SetBool(IsJumping, !_isGrounded);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("detail"))
+        if (other.CompareTag("ItemTag"))
         {
             string itemType = other.gameObject.GetComponent<CollectableScript>().itemType;
-            items.Add(itemType);
-            print(items.Count);
+            _items.Add(itemType);
             Destroy(other.gameObject);
         }
     }
